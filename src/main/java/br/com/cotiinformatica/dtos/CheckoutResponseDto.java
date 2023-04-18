ad5d70745719bca8fa6c42d049fed52bec568e63
Renato Areas <rsareas93@gmail.com>
2023-04-17 22:13:03 -0300

    primeiro commit

---
+++ src/main/java/br/com/cotiinformatica/dtos/CheckoutResponseDto.java
@@ -0,0 +1,21 @@
+package br.com.cotiinformatica.dtos;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckoutResponseDto {
+	
+	private String transactionId;
+	private String status;
+	private String message;
+	private OrderResponseDto order;
+	private BigDecimal total;
+
+
+}
---
+++ src/main/java/br/com/cotiinformatica/dtos/CreditCardRequestDto.java
@@ -0,0 +1,16 @@
+package br.com.cotiinformatica.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreditCardRequestDto {
+	private String cardNumber;
+	private String cardholderName;
+	private String expirationMonth;
+	private String expirationYear;
+	private String securityCode;
+}
